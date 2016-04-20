eae67f70e5615e4bbb3efe359e8e296caf3513d5
Gaetano Checinski <Gaetano.Checinski@gmail.com>
2016-04-20 20:31:58 +0100

    Update index.js

--- examples/index.js
+++ examples/index.js
@@ -1,7 +1,7 @@
 var Cor = require('babel-plugin-operator-overloading/runtime');
 
 function sum(a,b){
-return a + b + 5;
+  return a + b;
 }
 
-console.log( sum( {add:(x)=>x*10} , 2 ) ); //25
+console.log( sum( {add:(x)=>x*10} , 2 ) ); //20
